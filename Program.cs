b48f2b085f67595e43e8a73da67f80234eb7766c
misha-solntcev <misha.solntcev@gmail.com>
2022-12-06 19:00:37 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 6. Заменить отрицательные элементы в числовом массиве из n 
+    чисел (n>10) их квадратами, оставив остальные без изменения. */
+
+namespace Lab_4_1_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            Random r = new Random();
+            for (int i = 0; i < arr.Length; i++) 
+                arr[i] = r.Next(-10, 10);
+            foreach (var item in arr)
+                Console.Write(item + " ");
+            Console.WriteLine();
+
+            for (int i = 0; i < arr.Length; i++)            
+                if (arr[i] < 0)                
+                    arr[i] = arr[i] * arr[i];            
+            foreach (var item in arr)
+                Console.Write(item + " ");
+
+            Console.ReadKey();
+        }
+    }
+}
